34bb28835ec04a13f41daa8f81c17590751f70ac
NPWR <npwrmusic@gmail.com>
2015-07-20 13:02:32 +0200

    Fixed stupid error, now runs away from nearest enemy

--- bot.user.js
+++ bot.user.js
@@ -976,10 +976,12 @@ console.log("Running Apos Bot!");
 							var angle = getAngle(allPossibleThreats[i].x, allPossibleThreats[i].y, player[k].x, player[k].y);
 							angleWeights.push([angle,dist]);
 						}
-						var maxDist = 0;
+						
+						var maxDist = 10000;
 						var finalAngle = 0;
+						
 						for (var i = 0; i < angleWeights.length; i++){
-							if (angleWeights[i][1] > maxDist){
+							if (angleWeights[i][1] < maxDist){
 								maxDist = angleWeights[i][1];
 								finalAngle = (angleWeights[i][0] + 180).mod(360);
 							}
@@ -987,6 +989,7 @@ console.log("Running Apos Bot!");
 						var line1 = followAngle(finalAngle,player[k].x,player[k].y,f.verticalDistance());
                         drawLine(player[k].x, player[k].y, line1[0], line1[1], 2);
 						destinationChoices.push(line1);
+						
                     } else if (clusterAllFood.length > 0) {
                         for (var i = 0; i < clusterAllFood.length; i++) {
                             //console.log("mefore: " + clusterAllFood[i][2]);
